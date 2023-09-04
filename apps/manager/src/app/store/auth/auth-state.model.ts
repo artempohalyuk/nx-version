f68e82050c10759c8aaf479daae904a0406c07bc
Artem Pokhaliuk <artempohalyuk@gmail.com>
2023-09-04 09:10:29 +0300

    remove commented code

--- apps/manager/src/app/store/auth/auth-state.model.ts
+++ apps/manager/src/app/store/auth/auth-state.model.ts
@@ -3,6 +3,4 @@ import { IUser } from "@models";
 export interface IAuthState {
     user: IUser | null;
     isLoading: boolean | null;
-    // token: string | null;
-    // isLoggedIn: boolean | null;
 }
