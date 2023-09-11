bfdff57e611e018b906c70e369a8288b1ed32222
Artem Pokhaliuk <artempohalyuk@gmail.com>
2023-09-11 12:40:25 +0300

    changed api endpoints

--- apps/auth/src/environments/environment.ts
+++ apps/auth/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment: IEnvironment = {
   production: false,
-  // apiEndpoint: `https://server-microfrontend.vercel.app/api`,
-  apiEndpoint: `http://localhost:5000/api`
+  apiEndpoint: `https://server-microfrontend.vercel.app/api`,
 };
